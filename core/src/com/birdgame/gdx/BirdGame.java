8208ed128727259edff8d9187163edc4241f6dad
Aryan Kumar <aryan68125@gmail.com>
2020-12-07 07:38:37 +0530

    version 2

--- core/src/com/birdgame/gdx/BirdGame.java
+++ core/src/com/birdgame/gdx/BirdGame.java
@@ -70,6 +70,19 @@ public class BirdGame extends ApplicationAdapter {
 	int bombCount;
 	Random random2;
 
+	/**-------BIRDS THAT WILL MOVE NOT ONLY IN X AXIS BUT ALSO IN Y AXIS---------**/
+	//it will hold a bunch of bomb2 objects that are gonna be appearing on the screen
+	//creating an arrayList that will hold the coordinates of our bomb
+	ArrayList<Integer> bombX2 = new ArrayList<>();
+	ArrayList<Integer> bombY2 = new ArrayList<>();
+	//ArrayList of type Rectangle allows us add a shape to our objects on the screen
+	ArrayList<Rectangle> bombRectangles2 = new ArrayList<Rectangle>();
+	Texture bomb2;
+	//bombCount  =  proper spacing between the bomb
+	//it will allow us to show a bomb after every 100 loop of screen render
+	int bombCount2;
+	Random random3;
+
 	//int score will keep track of our score
 	int score =0;
 	//now to display our score onto our screen
@@ -143,6 +156,10 @@ public class BirdGame extends ApplicationAdapter {
 		bomb= new Texture("bird0.png");
 		random2 = new Random();
 
+		/**---SETTING UP BOMB DIRECTION IN X AND Y MOVENMENT  -------------**/
+		bomb2 = new Texture("bug.png");
+		random3 = new Random();
+
 		//setting our BitmapFont so that we can show the score onto our screen
 		Font = new BitmapFont();
 		//setting up the color on our BitmapFont
@@ -175,11 +192,24 @@ public class BirdGame extends ApplicationAdapter {
 		//float height = height at which the bomb will appear on the screen (randomly)
 		//random.nextFloat() = it will give us randome numbers between 0 and 1
 		//Gdx.graphics.getHeight() = will give us the height of the screen
-		float height = random2.nextFloat() * Gdx.graphics.getHeight();
+		//float height = random2.nextFloat() * max height at which a bomb can appear randomly;
+		float height = random2.nextFloat() * 800;
 		//now updating the coordinates of the bomb in the ArrayList that holds the coin Coordinates on the screen
 		bombY.add((int)height);
 		//make sure that your bomb appears off of the screen and the X coordinate of the coins will all be the same
 		bombX.add(Gdx.graphics.getWidth());
+	}
+
+	public void makebomb2(){
+		//float height = height at which the bomb will appear on the screen (randomly)
+		//random.nextFloat() = it will give us randome numbers between 0 and 1
+		//Gdx.graphics.getHeight() = will give us the height of the screen
+		//float height = random2.nextFloat() * max height at which a bomb can appear randomly;
+		float height = random3.nextFloat() * 800;
+		//now updating the coordinates of the bomb in the ArrayList that holds the coin Coordinates on the screen
+		bombY2.add((int)height);
+		//make sure that your bomb appears off of the screen and the X coordinate of the coins will all be the same
+		bombX2.add(Gdx.graphics.getWidth());
 	}
 
 	public void makeBullet(){
@@ -212,22 +242,29 @@ public class BirdGame extends ApplicationAdapter {
 		//if our gameState is equal to 1 then the game will behave normally
 		//if our gameState =0 then we are at the starting position
 		//if our gameState = 2 then game over
-		if(gameState == 1){
+		if(gameState == 1) {
 			//setting up our bombs
-			if(bombCount<250){
+			if (bombCount < 100) {
 				bombCount++;
-			}
-			else{
+			} else {
 				bombCount = 0;
 				//making a coin after every 100 frames rendered
 				makebomb();
 			}
 
+			//setting up our bombs2
+			if (bombCount2 < 300) {
+				bombCount2++;
+			} else {
+				bombCount2 = 0;
+				//making a coin after every 100 frames rendered
+				makebomb2();
+			}
+
 			bombRectangles.clear();
 			//using a for loop to draw our bombs on the screen
-			for(int i=0;i<bombX.size();i++)
-			{
-				batch.draw(bomb,bombX.get(i),bombY.get(i));
+			for (int i = 0; i < bombX.size(); i++) {
+				batch.draw(bomb, bombX.get(i), bombY.get(i));
 				//inorder to move the coin in the x directrion from right to left we need to update the X coordinate of the bomb
 				//updating the x coordinate of our bomb
 				//bombX.set(i, bombX.get(i)-4);
@@ -235,21 +272,41 @@ public class BirdGame extends ApplicationAdapter {
 				//current position = i
 				//bombX.get(i)-4 = moves bombs from right to left the speed can be controlled by changing the number four
 				//higher the number faster the bomb moves on the screen
-				bombX.set(i, bombX.get(i)-10);
+				bombX.set(i, bombX.get(i) - 10);
 
 				//coinRectangles.add(new Rectangle(coinX.get(i), coinY.get(i), coin.getWidth(),coin.getHeight())); = now adding the coin physX onto our coins
 				//new Rectangle(coordinates of the screen, size of the coin on the screen) = it will set the Rectangle on to our coins
 				//coinX.get(i), coinY.get(i) = telling the rectangle to appear where the coin appear on the screen
 				//coin.getWidth(),coin.getHeight() = the size of the rectangle should be equal to the size of the coin
-				bombRectangles.add(new Rectangle(bombX.get(i), bombY.get(i), bomb.getWidth(),bomb.getHeight()));
+				bombRectangles.add(new Rectangle(bombX.get(i), bombY.get(i), bomb.getWidth(), bomb.getHeight()));
+			}
+
+			/**SETTING UP BOMB 2 RECTANGLES AND DRAWING BOMB 2-----------**/
+			bombRectangles2.clear();
+			//using a for loop to draw our bombs on the screen
+			for (int i = 0; i < bombX2.size(); i++) {
+				batch.draw(bomb2, bombX2.get(i), bombY2.get(i));
+				//inorder to move the coin in the x directrion from right to left we need to update the X coordinate of the bomb
+				//updating the x coordinate of our bomb
+				//bombX.set(i, bombX.get(i)-4);
+				//bombX.set(); ->updates the x coordinates of a bomb
+				//current position = i
+				//bombX.get(i)-4 = moves bombs from right to left the speed can be controlled by changing the number four
+				//higher the number faster the bomb moves on the screen
+				bombX2.set(i, bombX2.get(i) - 20);
+
+				//coinRectangles.add(new Rectangle(coinX.get(i), coinY.get(i), coin.getWidth(),coin.getHeight())); = now adding the coin physX onto our coins
+				//new Rectangle(coordinates of the screen, size of the coin on the screen) = it will set the Rectangle on to our coins
+				//coinX.get(i), coinY.get(i) = telling the rectangle to appear where the coin appear on the screen
+				//coin.getWidth(),coin.getHeight() = the size of the rectangle should be equal to the size of the coin
+				bombRectangles2.add(new Rectangle(bombX2.get(i), bombY2.get(i), bomb2.getWidth(), bomb2.getHeight()));
 			}
 
 			//here we want to keep track of the character on what position our character man is in the game screen
 			//setting up the pause function in our app so that we can slow our games animation to a manageble speed
-			if(pause<5){
+			if (pause < 5) {
 				pause++;
-			}
-			else {
+			} else {
 				//here we will update our character's running animation
 				pause = 0;
 				//we have to have a way to draw our character so that we can show our character is running
@@ -261,10 +318,9 @@ public class BirdGame extends ApplicationAdapter {
 					manState = 0;
 				}
 			}
-			if(pause2<1){
+			if (pause2 < 1) {
 				pause2++;
-			}
-			else {
+			} else {
 				pause2 = 0;
 				//for firing plane
 				if (manState2 < 4) {
@@ -279,9 +335,9 @@ public class BirdGame extends ApplicationAdapter {
 			velocity = velocity + gravity;
 			manY -= velocity;
 			//we will set our character to remain on the ground and stop his decent once he has reached the ground
-			if(manY <= 3){
+			if (manY <= 3) {
 				manY = 3;
-			}else if(manY>=750){
+			} else if (manY >= 750) {
 				manY = 750; //it will prevent our character from going out of our screen
 			}
 
@@ -294,38 +350,35 @@ public class BirdGame extends ApplicationAdapter {
         4. it will look something like this:-position on the screen of our character-> x: Gdx.graphics.getWidth()/2, y: Gdx.graphics.getHeight()/2
         5. subtracting the half of  width of the man from the x: Gdx.graphics.getWidth()/2 (man's position) - man[0].getHeight()/2 (width of the man's image itself)
          */
-			batch.draw(man[manState],manX, manY);
+			batch.draw(man[manState], manX, manY);
 			//setting up a geometric shape for our character
-			manRectangle = new Rectangle(manX, manY,man[manState].getWidth(),man[manState].getHeight());
+			manRectangle = new Rectangle(manX, manY, man[manState].getWidth(), man[manState].getHeight());
 
 			//setting up the code so that our character can jump when the user touches him
 			//Gdx.input.justTouched() = it will handle all the touch input from the user in our game
 			//adding accelerometer support
 			//getting values from accelerometer
-			int Yaxis =(int) Gdx.input.getAccelerometerX();
-			Gdx.app.log("Y",Float.toString(Yaxis));
-			if(Yaxis<6){
+			int Yaxis = (int) Gdx.input.getAccelerometerX();
+			Gdx.app.log("Y", Float.toString(Yaxis));
+			if (Yaxis < 6) {
 				//velocity = -10; -> will make our character go up
 				velocity = -8;
-			}
-			else
-			{
+			} else {
 				// will make our charatcer go down
 				velocity = 8;
 			}
 
 			//checking condition for shooting guns
-			if(Gdx.input.isTouched()){
+			if (Gdx.input.isTouched()) {
 				//drawing firing plane after the normal plane
-				batch.draw(manfire[manState2],manX, manY);
+				batch.draw(manfire[manState2], manX, manY);
 				//setting up a geometric shape for our character
-				manRectangle2 = new Rectangle(manX, manY,manfire[manState2].getWidth(),manfire[manState2].getHeight());
+				manRectangle2 = new Rectangle(manX, manY, manfire[manState2].getWidth(), manfire[manState2].getHeight());
 
-					//playing shoot.mp3 when the screen is touched
-				if(pause3<7){ //delaying sound play
+				//playing shoot.mp3 when the screen is touched
+				if (pause3 < 7) { //delaying sound play
 					pause3++;
-				}
-				else {
+				} else {
 					pause3 = 0;
 					//for firing plane
 					if (soundState < 1) {
@@ -333,8 +386,8 @@ public class BirdGame extends ApplicationAdapter {
 					} else {
 						soundState = 0;
 						long id = sound.play(Float.parseFloat("1.0f"));
-						sound.setPitch(id,5);
-						sound.setLooping(id,false);
+						sound.setPitch(id, 5);
+						sound.setLooping(id, false);
 					}
 					//making bullets
 					makeBullet();
@@ -344,9 +397,8 @@ public class BirdGame extends ApplicationAdapter {
 			//setting up our bullets
 			bulletRectangles.clear();
 			//using a for loop to draw our bullets on the screen
-			for(int i=0;i<bulletX.size();i++)
-			{
-				batch.draw(bullet,bulletX.get(i),bulletY.get(i));
+			for (int i = 0; i < bulletX.size(); i++) {
+				batch.draw(bullet, bulletX.get(i), bulletY.get(i));
 				//inorder to move the coin in the x directrion from right to left we need to update the X coordinate of the bomb
 				//updating the x coordinate of our bomb
 				//bombX.set(i, bombX.get(i)-4);
@@ -354,29 +406,28 @@ public class BirdGame extends ApplicationAdapter {
 				//current position = i
 				//bombX.get(i)-4 = moves bombs from right to left the speed can be controlled by changing the number four
 				//higher the number faster the bomb moves on the screen
-				bulletX.set(i, bulletX.get(i)+15);
+				bulletX.set(i, bulletX.get(i) + 20);
 
 				//coinRectangles.add(new Rectangle(coinX.get(i), coinY.get(i), coin.getWidth(),coin.getHeight())); = now adding the coin physX onto our coins
 				//new Rectangle(coordinates of the screen, size of the coin on the screen) = it will set the Rectangle on to our coins
 				//coinX.get(i), coinY.get(i) = telling the rectangle to appear where the coin appear on the screen
 				//coin.getWidth(),coin.getHeight() = the size of the rectangle should be equal to the size of the coin
-				bulletRectangles.add(new Rectangle(bulletX.get(i), bulletY.get(i), bullet.getWidth(),bullet.getHeight()));
-				Gdx.app.log("bulletRec","bullet rectangles = "+bulletRectangles.get(i));
+				bulletRectangles.add(new Rectangle(bulletX.get(i), bulletY.get(i), bullet.getWidth(), bullet.getHeight()));
+				Gdx.app.log("bulletRec", "bullet rectangles = " + bulletRectangles.get(i));
 			}
 
 			//after we've updated our score now we have to draw the score on our screen
 			//drawing a Bitmap on the screen is a little bit different than drawing your game assets on the screen
-			Font.draw(batch,String.valueOf(score),100,1000);
+			Font.draw(batch, String.valueOf(score), 100, 1000);
 
 			//now setting up the collision mechanism for birds after everything is drawn
 			//using an if statement to check whether our character is colliding with the bomb or not
 			//for lop to get to all of our bomb
-			for(int i=0;i<bombRectangles.size();i++)
-			{
+			for (int i = 0; i < bombRectangles.size(); i++) {
 				//Intersector.overlaps() it checks whether the two images that have the designated polygons assigned to them have collided or not
-				if(Intersector.overlaps(manRectangle, bombRectangles.get(i))){
+				if (Intersector.overlaps(manRectangle, bombRectangles.get(i))) {
 					//so lets go ahead and log some information here
-					Gdx.app.log("Bomb","Collision with bomb");
+					Gdx.app.log("Bomb", "Collision with bomb");
 
 					//getting rid of the bomb after the collision with the bomb of our character
 					bombRectangles.remove(i);
@@ -385,33 +436,83 @@ public class BirdGame extends ApplicationAdapter {
 
 					//playing crash sound when the plane is collided with the bird
 					long id = crash.play(Float.parseFloat("1.0f"));
-					crash.setLooping(id,false);
+					crash.setLooping(id, false);
 
 					//updating our gameState
 					gameState = 2;
 					break;
 				}
-			}
 
-			//checking the collision between the bullets and the birds
-			int bul;
-			for(int b = 0 ; b<bombRectangles.size();b++){
-				for(bul = 0;bul<bulletRectangles.size();bul++) {
-					if (Intersector.overlaps(bulletRectangles.get(bul), bombRectangles.get(b))) {
+				/** -------SETTING UP COLLISION MECHANISM FOR BOMB2-----**/
+				//now setting up the collision mechanism for birds after everything is drawn
+				//using an if statement to check whether our character is colliding with the bomb or not
+				//for lop to get to all of our bomb
+				for (int j = 0; j < bombRectangles2.size(); j++) {
+					//Intersector.overlaps() it checks whether the two images that have the designated polygons assigned to them have collided or not
+					if (Intersector.overlaps(manRectangle, bombRectangles2.get(j))) {
+						//so lets go ahead and log some information here
+						Gdx.app.log("Bomb2", "Collision with bomb2");
+
 						//getting rid of the bomb after the collision with the bomb of our character
-						bombRectangles.remove(b);
-						bombX.remove(b);
-						bombY.remove(b);
-						score++;
-						if(score>HighScore){
-							HighScore=score;
-						}
-						gameState = 1;
+						bombRectangles2.remove(j);
+						bombX2.remove(j);
+						bombY2.remove(j);
+
+						//playing crash sound when the plane is collided with the bird
+						long id = crash.play(Float.parseFloat("1.0f"));
+						crash.setLooping(id, false);
+
+						//updating our gameState
+						gameState = 2;
 						break;
 					}
 				}
-			}
 
+				//checking the collision between the bullets and the birds
+				int bul;
+				for (int b = 0; b < bombRectangles.size(); b++) {
+					for (bul = 0; bul < bulletRectangles.size(); bul++) {
+						if (Intersector.overlaps(bulletRectangles.get(bul), bombRectangles.get(b))) {
+							//getting rid of the bomb after the collision with the bomb of our character
+							bombRectangles.remove(b);
+							bombX.remove(b);
+							bombY.remove(b);
+							bulletRectangles.remove(bul);
+							bulletX.remove(bul);
+							bulletY.remove(bul);
+							score++;
+							if (score > HighScore) {
+								HighScore = score;
+							}
+							gameState = 1;
+							break;
+						}
+					}
+
+					//checking the collision between the bullets and the bugs (bomb2)
+					int bul2;
+					for (int b2 = 0; b2 < bombRectangles2.size(); b2++) {
+						for (bul2 = 0; bul2 < bulletRectangles.size(); bul2++) {
+							if (Intersector.overlaps(bulletRectangles.get(bul2), bombRectangles2.get(b2))) {
+								//getting rid of the bomb after the collision with the bomb of our character
+								bombRectangles2.remove(b2);
+								bombX2.remove(b2);
+								bombY2.remove(b2);
+								bulletRectangles.remove(bul2);
+								bulletX.remove(bul2);
+								bulletY.remove(bul2);
+								score++;
+								if (score > HighScore) {
+									HighScore = score;
+								}
+								gameState = 1;
+								break;
+							}
+						}
+					}
+
+				}
+			}
 		}
 		else if(gameState == 0)
 		{
